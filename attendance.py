a401ad708f84fa46c9e81964be9e615d9f616f3c
Curtis Mason <cumason@bu.edu>
2020-02-13 17:51:07 -0500

    Created packet handling thread.

--- attendance.py
+++ attendance.py
@@ -1,23 +1,69 @@
 import pyshark as ps
 import os
+from threading import Thread
+import queue
 capture = ps.LiveCapture(interface='en0', display_filter='eapol')
-emails = set()
+identities = set()
+packets_buffer = queue.Queue()
 
-while True:
-	print("Sniffing for packets")
-	capture.sniff(packet_count=1)
-	handler = open('bu-users.txt', 'w')
-	for packet in capture:
+
+def update_users(identity_list='./identities.txt'):
+	""" 
+	Flushes identity strings to the identity_list file
+
+	Parameters
+	----------
+	identity_list : str
+		string representing filepath to flush identities to
+	"""
+	with open(identity_list, 'w') as file:
+		file.write('\n'.join(identities))
+
+
+def layer2identity(layer):
+	"""
+	Extracts identity header from a packet layer. Assumes layer
+	has identity field.
+
+	Parameters
+	----------
+	layer : obj
+		layer object you can get from iterating through a pyshark packet
+	"""
+	identity = layer.get_field('identity')
+	if not identity.endswith('@bu.edu'):
+		identity += '@bu.edu'
+	return identity
+
+
+def packet_handler():
+	"""
+	Takes packets from a queue and outputs it to some filepath
+	"""
+	while True:
+		packets_buffer.get()
 		print("Gotcha!", list(packet))
-
 		for layer in packet:
 			if ('identity' in layer.field_names):
-				email = layer.get_field('identity')
-				if not email.endswith('@bu.edu'):
-					email += '@bu.edu'
-				print("New Identity: {0}".format(email))
-				emails.add(email)
+				identity = layer2identity(layer)
+				print("New Identity: {0}".format(identity))
+				identities.add(identity)
+				update_users()
 			else:
 				print('Identity not found in packet with: {0}'.format(layer))
-	handler.write('\n'.join(emails))
-	handler.close()
+
+
+def take_attendance():
+	"""
+	Logs attendance by sniffing packets on a 802 network and
+	extracting identity headers from corresponding eapol protocol's. 
+	"""
+	packet_handler_thread = Thread()
+	packet_handler_thread.run()
+	while True:
+		print("Sniffing for packets")
+		capture.sniff(packet_count=1) # buggy setup separate thread to handle this stuff
+		
+
+if __name__ == '__main__':
+	take_attendance()
